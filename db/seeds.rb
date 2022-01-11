d029294ba43461a3bc337c321468ec91298c2bab
Clay Dunston <claydunston@gmail.com>
2022-01-11 09:16:00 -0600

    Allow arguments for rails db:seed so we can seed specific tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,12 @@
-Lib::Tasks::Seed.all()
+entity = ENV.fetch("entity", :none).to_sym()
+
+if entity == :none
+  Lib::Tasks::Seed.all()
+end
+
+begin
+  Lib::Tasks::Seed.send(entity)
+rescue StandardError => e
+  binding.pry()
+  puts(e)
+end
