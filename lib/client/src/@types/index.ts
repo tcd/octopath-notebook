144aaff63508dbc009de0b2fecbe61ac06a73f2b
Clay Dunston <claydunston@gmail.com>
2022-01-15 14:16:37 -0600

    move model types to subfolder

--- lib/client/src/@types/index.ts
+++ lib/client/src/@types/index.ts
@@ -1,12 +1,1 @@
-export * from "./ApplicationRecord"
-
-export * from "./Character"
-export * from "./DamageType"
-export * from "./Equipment"
-export * from "./EquipmentCategory"
-export * from "./Job"
-export * from "./JobSkill"
-export * from "./Party"
-export * from "./PartyCharacter"
-export * from "./Stat"
-export * from "./WeaponType"
+export * from "./models"
