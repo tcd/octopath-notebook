a7a027a5e27fafe634b1ee59d8f7a8053881e4c3
Clay Dunston <claydunston@gmail.com>
2022-01-22 14:53:06 -0600

    Update font folders

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,8 @@
 //= link_tree ../images
-//= link_tree ../fonts
+
+//= link_tree ../fonts/material_design_icons
+//= link_tree ../fonts/rpg_awesome
+//= link_tree ../fonts/sketch_block
+
 //= link_directory ../stylesheets .scss
 //= link_directory ../javascripts/react .js
